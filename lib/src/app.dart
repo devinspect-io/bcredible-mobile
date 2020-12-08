0c8e878bffa40661ebc5a60d2af7d2077ecc187e
sohail0992 <msohail.se@gmail.com>
2020-12-09 04:02:45 +0500

    signup flow

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -4,33 +4,33 @@ import 'package:flutter/material.dart';
 import 'blocs/login_bloc.dart';
 import 'screens/signup_screen.dart';
 
-// class App extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//         debugShowCheckedModeBanner: false,
-//         home: Scaffold(
-//           appBar: AppBar(
-//               title: Text('bCredible'),
-//               backgroundColor: Color.fromRGBO(0, 209, 189, 100)),
-//           body: HomeScreen(),
-//         ));
-//   }
-// }
-
-// for login
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Provider(
-      child: MaterialApp(
-        title: 'Log Me In',
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-            appBar: AppBar(
-                title: Text('bCredible'),
-                backgroundColor: Color.fromRGBO(0, 209, 189, 100)),
-            body: LoginScreen()),
-      ),
-    );
+          appBar: AppBar(
+              title: Text('bCredible'),
+              backgroundColor: Color.fromRGBO(0, 209, 189, 100)),
+          body: HomeScreen(),
+        ));
   }
 }
+
+// // for login
+// class App extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Provider(
+//       child: MaterialApp(
+//         title: 'Log Me In',
+//         home: Scaffold(
+//             appBar: AppBar(
+//                 title: Text('bCredible'),
+//                 backgroundColor: Color.fromRGBO(0, 209, 189, 100)),
+//             body: LoginScreen()),
+//       ),
+//     );
+//   }
+// }
